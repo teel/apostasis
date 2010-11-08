c9781f4b723141baceb1ac2d39e4acaebc7602ee
eddedmondson <eddedmondson@dc9508a2-aa8a-11de-b6f4-e1be98fab775>
2010-11-08 13:36:46 +0000

    Added logfile merging tool

--- lib/Stasis/Page/Chart.pm
+++ lib/Stasis/Page/Chart.pm
@@ -850,11 +850,13 @@ $deathLegendAddition
         // clamp the zooming to prevent eternal zoom
         if (ranges.xaxis.to - ranges.xaxis.from < 0.00001)
             ranges.xaxis.to = ranges.xaxis.from + 0.00001;
-        
+        if (ranges.yaxis.to - ranges.yaxis.from < 0.00001)
+            ranges.yaxis.to = ranges.taxis.from + 0.00001;
         // do the zooming
         mainplot = \$.plot(\$("#mainplot"),[ {data:dps,color:"rgb(255,0,0)", label:"Dmg out = -------"}, {data:heal,color:"rgb(0,0,255)", label:"Healing = -------"}, {data:din,color:"rgb(0,0,0)", label:"Dmg in = -------"} ],
                       \$.extend(true, {}, options, {
-                          xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
+                          xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
+                          yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
                       }));
         // reset legends
         legends = \$("#mainplot .legendLabel");
